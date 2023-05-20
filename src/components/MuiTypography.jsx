079cb7a1f26a276db3ad65fbf91555e68e0f0220
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-05-20 11:30:06 +0530

    Started learning react MUI

---
+++ src/components/MuiTypography.jsx
@@ -0,0 +1,31 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant='h1' >Hello- h1</Typography>
+        <Typography variant='h2' >Hello- h2</Typography>
+        <Typography variant='h3' >Hello- h3</Typography>
+        <Typography variant='h4' >Hello- h4</Typography>
+        <Typography variant='h5' >Hello- h5</Typography>
+        <Typography variant='h6' >Hello- h6</Typography>
+        <Typography variant='subtitle1' >sub-title- h6</Typography>
+        <Typography variant='subtitle2' >Hello- h6</Typography>
+        <Typography>
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sint eum neque! Quis quisquam incidunt eius, vel sunt, dolor excepturi praesentium aliquam porro a eveniet vero iste, nihil iusto quo.
+        </Typography>
+        <Typography variant='body1' >
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sint eum neque! Quis quisquam incidunt eius, vel sunt, dolor excepturi praesentium aliquam porro a eveniet vero iste, nihil iusto quo.
+        </Typography>
+        <Typography variant='body2' >
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sint eum neque! Quis quisquam incidunt eius, vel sunt, dolor excepturi praesentium aliquam porro a eveniet vero iste, nihil iusto quo.
+        </Typography>
+        <Typography variant='' >
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sint eum neque! Quis quisquam incidunt eius, vel sunt, dolor excepturi praesentium aliquam porro a eveniet vero iste, nihil iusto quo.
+        </Typography>
+    </div>
+  )
+}
+
+export default MuiTypography
